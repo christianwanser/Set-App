44e7bc78f4afbb3c5c33a0e7c18a30e6d8a3d058
cwanserGT <110735970+christianwanser@users.noreply.github.com>
2023-01-25 14:25:06 -0800

    Update solver_final.py

--- solver_final.py
+++ solver_final.py
@@ -481,11 +481,12 @@ def solveit(input_image):
 #    )
 #    args = parser.parse_args()
 
-    img = cv2.imread(input_image)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    annotated_img = annotate_sets_on_frame(img, debug=True)
-    plt.imshow(annotated_img)
-    plt.show()
+    if input_image:
+        img = cv2.imread(input_image)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        annotated_img = annotate_sets_on_frame(img, debug=True)
+        plt.imshow(annotated_img)
+        plt.show()
 
 
 #if __name__ == "__main__":
